7aadbb7e4d103542224d2ec30af6e63a40a96b6d
Aswath Pranav Balan T <ashd2607@gmail.com>
2025-11-01 23:15:14 +0530

    11:15 Update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-// app.js
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 80;
+const http = require('http');
+const port = 80;
 
-app.get('/', (req, res) => res.send('Hello from CI/CD demo on Azure!'));
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from CI/CD demo on Azure!');
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
