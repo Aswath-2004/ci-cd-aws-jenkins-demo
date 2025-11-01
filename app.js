f55c19b787b40fa840f1ba3106071af6808c2090
Aswath Pranav Balan <ashd2607@gmail.com>
2025-11-01 23:26:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 80;
 
-app.get('/', (req, res) => res.send('Hello from CI/CD demo on AWS!'));
+app.get('/', (req, res) => res.send('Hello from CI/CD demo on Azure!'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
