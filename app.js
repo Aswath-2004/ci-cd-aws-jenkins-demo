7cb5041cc2893e8f30d79fbf27c88147b878241f
Aswath Pranav Balan T <ashd2607@gmail.com>
2025-11-02 16:00:01 +0530

    2nd Nov update

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 80;
 
-app.get('/', (req, res) => res.send('Hello from CI/CD demo of Aswath Pranav Balan T of IoT A on Azure v2!'));
+app.get('/', (req, res) => res.send('Hello from CI/CD demo of Aswath Pranav Balan T of IoT A day 2 on Azure v2!'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
